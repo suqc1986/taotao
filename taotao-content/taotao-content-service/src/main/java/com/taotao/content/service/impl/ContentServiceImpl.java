a9a2cce6fcc75494dca3bb17eec9c8d39672d308
sqc <sqc1986@163.com>
2017-12-18 22:49:47 +0800

    学习淘淘商城第三十四课（在业务逻辑中添加缓存）

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,14 +3,18 @@ package com.taotao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.service.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
@@ -20,6 +24,12 @@ import com.taotao.pojo.TbContentExample.Criteria;
 public class ContentServiceImpl implements ContentService {  
     @Autowired  
     private TbContentMapper contentMapper;  
+    
+    @Autowired
+    private JedisClient jedisClient;
+    
+    @Value("${INDEX_CONTENT")
+    private String INDEX_CONTENT;
   
     @Override  
     public EasyUIDataGridResult getContentList(long categoryId, int page, int rows) {  
@@ -41,11 +51,13 @@ public class ContentServiceImpl implements ContentService {
   
     @Override  
     public TaotaoResult addContent(TbContent content) {  
-        //补充属性  
+    	  //补充属性  
         content.setCreated(new Date());  
         content.setUpdated(new Date());  
         //添加  
         contentMapper.insert(content);  
+        //同步缓存，由于首页大广告位的分类ID为89，content.getCategoryId()得到的便是89  
+        jedisClient.hdel(INDEX_CONTENT, content.getCategoryId().toString());  
         //返回结果  
         return TaotaoResult.ok();  
     }  
@@ -79,11 +91,31 @@ public class ContentServiceImpl implements ContentService {
 
 	@Override
 	public List<TbContent> getContentListByCid(long cid) {
-		 TbContentExample example = new TbContentExample();  
-	        Criteria criteria = example.createCriteria();  
-	        criteria.andCategoryIdEqualTo(cid);  
-	        List<TbContent> list = contentMapper.selectByExample(example);  
-	        return list;  
+		//首先查询缓存，如果缓存中存在的话，就直接将结果返回给前台展示，查询缓存不能影响业务流程  
+        try {  
+            String json = jedisClient.hget(INDEX_CONTENT, cid+"");  
+            //如果从缓存中查到了结果  
+            if(StringUtils.isNotBlank(json)){  
+                //将json串转化为List<TbContent>  
+                List<TbContent> list = JSON.parseArray(json, TbContent.class);  
+                return list;  
+            }  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        TbContentExample example = new TbContentExample();    
+        Criteria criteria = example.createCriteria();    
+        criteria.andCategoryIdEqualTo(cid);    
+        List<TbContent> list = contentMapper.selectByExample(example);    
+        //添加缓存，不能影响业务流程  
+        try {  
+            String json = JSON.toJSONString(list);  
+            jedisClient.hset(INDEX_CONTENT, cid+"", json);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        //返回结果  
+        return list;   
 	}  
   
 }  
