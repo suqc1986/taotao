1e23a32136de8d8dd3b7f21065095e7da0ef20d2
sqc <sqc1986@163.com>
2017-12-20 22:55:03 +0800

    学习淘淘商城第四十五课（使用SolrJ查询索引库）

--- taotao-search/taotao-search-service/src/test/java/com/taotao/search/test/TestSolrJ.java
+++ taotao-search/taotao-search-service/src/test/java/com/taotao/search/test/TestSolrJ.java
@@ -1,5 +1,8 @@
 package com.taotao.search.test;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
@@ -65,4 +68,49 @@ public class TestSolrJ {
 	        System.out.println(title);  
 	    }  
 	} 
+	@Test  
+    public void queryDocument2() throws Exception{  
+        //创建一个SolrServer对象，创建一个HttpSolrServer对象，需要指定solr服务的url  
+        SolrServer solrServer = new HttpSolrServer(URL);  
+        //创建一个SolrQuery对象  
+        SolrQuery query = new SolrQuery();  
+        //设置查询条件、过滤条件、分页条件、排序条件、高亮  
+        //query.set("q", "*:*");  
+        query.setQuery("手机");  
+        //分页条件  
+        query.setStart(0);  
+        query.setRows(3);  
+        //设置默认搜索域  
+        query.set("df", "item_keywords");  
+        //设置高亮  
+        query.setHighlight(true);  
+        //高亮显示的域  
+        query.addHighlightField("item_title");  
+        query.setHighlightSimplePre("<em>");  
+        query.setHighlightSimplePost("</em>");  
+        //执行查询，得到一个Response对象  
+        QueryResponse response = solrServer.query(query);  
+        //取查询结果  
+        SolrDocumentList solrDocumentList = response.getResults();  
+        //取查询结果总记录数  
+        System.out.println("查询结果总记录数："+solrDocumentList.getNumFound());  
+        for(SolrDocument document : solrDocumentList){  
+            System.out.println(document.getFieldValue("id"));  
+            //取高亮显示  
+            Map<String,Map<String,List<String>>> highlighting = response.getHighlighting();  
+            List<String> list = highlighting.get(document.getFieldValue("id")).get("item_title");  
+            String itemTitle = "";  
+            if(list != null && list.size() > 0){  
+                itemTitle = list.get(0);  
+            }else {  
+                itemTitle = (String)document.get("item_title");  
+            }  
+            System.out.println(itemTitle);  
+            System.out.println(document.get("item_sell_point"));  
+            System.out.println(document.get("item_price"));  
+            System.out.println(document.get("item_image"));  
+            System.out.println(document.get("item_category_name"));  
+            System.out.println("===============================================");  
+        }  
+    }  
 }
