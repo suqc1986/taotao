d2faf4cf1423701cb3f980a0d7c2dad81792e386
sqc <sqc1986@163.com>
2018-01-07 01:34:01 +0800

    学习淘淘商城第八十八课（单点登录用户登录--Controller层及测试）

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.taotao.sso.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.CookieUtils;
 import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 
@@ -15,6 +20,8 @@ import com.taotao.sso.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
+	@Value("${TOKEN_KEY}")
+	private String TOKEN_KEY;
 
 	@RequestMapping("/user/check/{param}/{type}")
 	@ResponseBody
@@ -29,4 +36,14 @@ public class UserController {
 		TaotaoResult register = userService.register(tbUser);
 		return register;
 	}
+	
+	@RequestMapping(value = "/user/login",method = RequestMethod.POST)  
+    @ResponseBody  
+    public TaotaoResult login(String username,String password,  
+            HttpServletRequest request,HttpServletResponse response){  
+        TaotaoResult result = userService.login(username, password);  
+        //把token写入到cookie  
+        CookieUtils.setCookie(request, response, TOKEN_KEY, result.getData().toString());  
+        return result;  
+    }  
 }
