02002a6e1a7a1262cfd4849955025ff48b96f3ef
sqc <sqc1986@163.com>
2018-01-07 00:56:13 +0800

    学习淘淘商城第八十六课（单点登录用户注册Controller层实现及测试）

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -4,19 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
+
 	@RequestMapping("/user/check/{param}/{type}")
 	@ResponseBody
-	public TaotaoResult checkUserData(@PathVariable String param,@PathVariable Integer type){
+	public TaotaoResult checkUserData(@PathVariable String param, @PathVariable Integer type) {
 		TaotaoResult result = userService.checkUserData(param, type);
 		return result;
+	}
+
+	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult register(TbUser tbUser) {
+		TaotaoResult register = userService.register(tbUser);
+		return register;
 	}
 }
