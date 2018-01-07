bb865001c44aff77c7a22020660d00151ae02415
sqc <sqc1986@163.com>
2018-01-07 22:56:03 +0800

    学习淘淘商城第一百零三课（利用拦截器实现登录功能及订单确认页面展示）

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +12,8 @@ public class PageController {
 		return "register";
 	}
 	@RequestMapping("/login")
-	public String showLogin(){
+	public String showLogin(String url,Model model){
+		model.addAttribute("redirect", url);
 		return "login";
 	}
 }
